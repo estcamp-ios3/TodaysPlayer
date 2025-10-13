bcfd57b54df30b95893c728c17469a65b35f141e
JongHyuck-Lim <racek0801@gmail.com>
2025-10-13 09:43:12 +0900

    chore: 회원 탈퇴 버튼을 스크롤 뷰 안에 보이도록 수정

--- TodaysPlayer/TodaysPlayer/Scene/MyPage/SubViews/AccountView.swift
+++ TodaysPlayer/TodaysPlayer/Scene/MyPage/SubViews/AccountView.swift
@@ -21,18 +21,11 @@ struct AccountView: View {
         }
     }
     
-    var body: some View {
-        NavigationStack {
-            ScrollView {
-                pwEdit
-            }
-        }
-        
-        
+    private var checkOut: some View {
         Button {
             showDeleteAlert = true
         } label: {
-            MyPageRowView(icon: "person.slash.fill", iconColor: .red, title: "회원 탈퇴", subtitle: "회원 탈퇴 합니다.")
+            MyPageRowView(icon: "person.slash.fill", iconColor: .red, title: "회원 탈퇴", subtitle: "모든 정보를 지우고 회원 탈퇴 합니다.")
         }
         .alert("정말 탈퇴하시겠습니까?", isPresented: $showDeleteAlert) {
             Button("취소", role: .cancel) { }
@@ -64,6 +57,15 @@ struct AccountView: View {
                         .background(Color.white)
                         .cornerRadius(12)
                 }
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                pwEdit
+                checkOut
             }
         }
     }
